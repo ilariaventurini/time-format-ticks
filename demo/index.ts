049c272bac698807fa2fef199afc7feba0b172a8
Ilaria Venturini <venturini.ila@gmail.com>
2020-03-22 18:33:10 +0100

    📙Create demo page

--- demo/index.ts
+++ demo/index.ts
@@ -1,51 +1,105 @@
+import 'tachyons'
+import 'tachyons-extra'
+import 'style.css'
 import { scaleTime } from 'd3-scale'
 import { axisBottom } from 'd3-axis'
 import { extent } from 'd3-array'
 import { select } from 'd3-selection'
+import { getFormatter } from '../src/index'
+import { computeTimeIntervalName } from '../src/lib/computeTimeInterval'
+import { Options } from '../src/lib/types'
 
-////////////////////////
-
-type Margins = { top: number, right: number, bottom: number, left: number }
-type Dimensions = { width: number, height: number }
-
-////////////////////////
-
-function createAxis(holder: any, dataset: Date[], dimensions: Dimensions, margins: Margins, title: string) {
+function createExample(holder: any, title: string, dataset: any, options: Options): void {
   const { width, height } = dimensions
   const domain = extent(dataset)
   const range = [0, width - margins.left - margins.right]
   const scaleX = scaleTime().domain(domain).range(range)
-  const axis = axisBottom(scaleX)
+  const axisGenerator = axisBottom(scaleX)
 
-  const svg = holder
+  // get and set ticks formatter
+  const ticks = dataset.map(d => d.getTime())
+  const timeInterval = computeTimeIntervalName(ticks)
+  const formatter = getFormatter(ticks, options)
+  axisGenerator.tickFormat(formatter)
+
+  const container = `
+  <div id="container-${title}" class="ba" style="margin: ${containerMargin}; width: ${width}; background-color: #ffffff">
+    <div class="title" id="title-${title}" style="">
+      ${title}
+    </div>
+    
+    <div class="container" id="dataset-${title}" style="">
+      <div class="container-title" style="">Dataset</div>
+      <div class="code content" style="">
+        ${dataset.map((d, i) => `<div class="" style="width:${width - 2 * 2 * borderWidth}px">${d.toISOString()}</div>`).join('')}
+      </div>
+    </div>
+
+    <div class="container" id="options-${title}" style="">
+      <div class="container-title" style="">Options</div>
+      <div class="code content" style="">
+        ${options}
+      </div>
+    </div>
+    
+    <div class="container" id="time-interval-${title}" style="">
+      <div class="container-title" style="">Time interval</div>
+      <div class="content" style="">
+        ${timeInterval}
+      </div>
+    </div>
+    
+    <div class="container" id="axis-${title}" style="">
+      <div class="container-title" style="">Result</div>
+    </div>
+  </div>
+`
+
+  holder.append('div').html(container)
+
+  holder.select(`#axis-${title}`)
     .append('svg')
     .attr('width', width)
     .attr('height', height)
     .append('g')
     .attr('transform', `translate(${margins.left}, ${margins.top})`)
-
-  svg.append('g')
+    .append('g')
     .attr('width', width - margins.left - margins.right)
     .attr('height', height - margins.top - margins.bottom)
     .attr('transform', `translate(0, ${height - margins.top - margins.bottom})`)
-    .call(axis);
+    .call(axisGenerator)
 }
 
 ////////////////////////
 
-const body = select('body')
-const dimensions = { width: 600, height: 50 }
-const margins = { top: 20, right: 20, bottom: 20, left: 20 }
+const root = select('#app')
+const containerMargin = 20
+const borderWidth = 0
+const width = document.getElementById('app').clientWidth - 2 * containerMargin
+const dimensions = { width: width, height: 50 }
+const margins = { top: 20, right: 50, bottom: 20, left: 50 }
+const options = undefined
 
-const dates = [
-  new Date(2020, 11, 10, 22, 30),
-  new Date(2020, 11, 10, 23, 0),
-  new Date(2020, 11, 10, 23, 30),
-  new Date(2020, 11, 11, 0, 0),
-  new Date(2020, 11, 11, 0, 30),
-  new Date(2020, 11, 11, 1, 0),
-  new Date(2020, 11, 11, 1, 30),
-]
-
-createAxis(body, dates, dimensions, margins, 'Example #1')
-createAxis(body, dates, dimensions, margins, 'Example #2')
+createExample(root, '15seconds', [
+  new Date(2020, 11, 10, 23, 59, 15),
+  new Date(2020, 11, 10, 23, 59, 30),
+  new Date(2020, 11, 10, 23, 59, 45),
+  new Date(2020, 11, 11, 0, 0, 0),
+  new Date(2020, 11, 11, 0, 0, 15),
+  new Date(2020, 11, 11, 0, 0, 30),
+  new Date(2020, 11, 11, 0, 0, 45),
+], options)
+createExample(root, 'Minute', [
+  new Date(2020, 4, 21, 23, 47, 0),
+  new Date(2020, 4, 21, 23, 58, 0),
+  new Date(2020, 4, 21, 23, 59, 0),
+  new Date(2020, 4, 22, 0, 0, 0),
+  new Date(2020, 4, 22, 0, 1, 0),
+  new Date(2020, 4, 22, 0, 2, 0),
+  new Date(2020, 4, 22, 0, 3, 0),
+  new Date(2020, 4, 22, 11, 58, 0),
+  new Date(2020, 4, 22, 11, 59, 0),
+  new Date(2020, 4, 22, 12, 0, 0),
+  new Date(2020, 4, 22, 12, 1, 0),
+  new Date(2020, 4, 22, 12, 2, 0),
+], options)
