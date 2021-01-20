0682944bcc53d058095f30f65ab6a51339e0236f
Ivan Rossi <ivanross.me@gmail.com>
2021-01-20 08:21:39 +0100

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { getFormatter } from './lib/getFormatter'
 export { isTickPrimary } from './lib/formatTick'
 export { computeTimeIntervalName } from './lib/computeTimeInterval'
+export * from './lib/types'
